b703fd1bea0474e9eb5091424fc6ce34b7b22f29
TumDoka <149177290+TumDoka@users.noreply.github.com>
2024-05-10 13:31:48 +0300

    0.01-1.20.1

--- src/main/java/tumdoka/gems/item/bijouterie/JadeAmulet.java
+++
@@ -1,9 +0,0 @@
-package tumdoka.gems.item.bijouterie;
-
-import dev.emi.trinkets.api.TrinketItem;
-
-public class JadeAmulet extends TrinketItem {
-    public JadeAmulet(Settings settings) {
-        super(settings);
-    }
-}
--- src/main/java/tumdoka/gems/item/bijouterie/TopazRing.java
+++
@@ -1,9 +0,0 @@
-package tumdoka.gems.item.bijouterie;
-
-import dev.emi.trinkets.api.TrinketItem;
-
-public class TopazRing extends TrinketItem {
-    public TopazRing(Settings settings) {
-        super(settings);
-    }
-}
